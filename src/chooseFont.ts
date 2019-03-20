18bbc2256ed74ec2a0a5164cdbe155e73f4166c5
johncoffee <github2017@ragelse.dk>
2019-03-20 09:37:28 +0100

    urnesten shows bronze

--- src/chooseFont.ts
+++ src/chooseFont.ts
@@ -3,8 +3,13 @@ import { Font, IState } from './store'
 import { dispatchUpdateShorthand as upd } from './module1'
 
 export function render (state:IState) {
-  return html` 
-    <div @click=${{handleEvent: () =>upd({font:Font.Helvetica})}}>Helvetica</div>
-    <div @click=${{handleEvent: () =>upd({font:Font.Antikva})}}>Antikva</div>
+  return html`
+<div class="grid-x"> 
+    <div class="cell auto" @click=${()=>upd({font:Font.Helvetica }) }>${Font[Font.Helvetica]}</div>
+    <div class="cell auto" @click=${()=>upd({font:Font.Skriveskrift }) }>${Font[Font.Skriveskrift]}</div>
+    <div class="cell auto" @click=${()=>upd({font:Font.Antikva }) }>${Font[Font.Antikva]}</div>
+    ${state.order.graveCategory === 'urnesten' ? 
+      html`<div class="cell auto" @click=${()=>upd({font:Font.Bronze }) }>${Font[Font.Bronze]}</div>` : null}
+</div>
     `
 }
